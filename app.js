54eea8a48fa11470bcce79221224ee155d201cb4
nemracw <nemracw@gmail.com>
2021-12-17 17:54:59 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Url = require('./models/url')
 
 
 
@@ -23,7 +24,10 @@ app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Url.find() // 取出 Url model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(url => res.render('index', { url })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error))  // 錯誤處理
 })
 
 app.listen(3000, () => {
