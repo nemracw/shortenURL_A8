2936b11c459a9af3ba6a0de7b3dd548f6272ce68
nemracw <nemracw@gmail.com>
2021-12-17 16:19:24 +0800

    feat: add mongoose

---
+++ models/seeds/urlSeeder.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Todo = require('../url') // 載入 urk model
+mongoose.connect('mongodb://localhost/', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+  console.log('done')
+})
+
