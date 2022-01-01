54156dbb8bc2115dda8a99d157d36e9dd5ea5873
nemracw <nemracw@gmail.com>
2022-01-02 01:55:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,30 +4,18 @@ const methodOverride = require('method-override')
 const port = 3000
 
 const routes = require('./routes/router')
+require('./config/mongoose')
 const app = express()
-
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/urlShortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-app.use(express.static('public')) // setting static files
-app.use(express.urlencoded({ extended: true })) // setting body-parser
 
 // handlebars settings
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public')) // setting static files
+app.use(express.urlencoded({ extended: true })) // setting body-parser
 app.use(methodOverride('_method'))
+
 app.use(routes)
-
-
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,19 +1,3 @@
-const mongoose = require('mongoose')
-
 const Url = require('../url') // 載入 url model
-
-mongoose.connect('mongodb://localhost/urlShortener', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('../../config/mongoose')
 
