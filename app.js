5f8c8b42b1a13225336ac08d6f2ac61e28accc17
nemracw <nemracw@gmail.com>
2022-01-02 01:44:23 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override') 
-
-const app = express()
 const port = 3000
 
+const routes = require('./routes/router')
+const app = express()
+
 const mongoose = require('mongoose')
-
-const Url = require('./models/url')
-const generatePath = require('./public/javascripts/generate_shortURL')
-const host = 'www.localhost:3000/'
-
-
-app.use(express.static('public')) // setting static files
-app.use(express.urlencoded({ extended: true })) // setting body-parser
 
 mongoose.connect('mongodb://localhost/urlShortener', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -24,47 +17,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(express.static('public')) // setting static files
+app.use(express.urlencoded({ extended: true })) // setting body-parser
+
 // handlebars settings
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/shortened', (req, res) => {
-  const targetURL = req.body.inputURL
-  const shortUrl = host + generatePath(5)
-  Url.findOne({ originalUrl: targetURL })
-    .then(data =>
-      data ? data : Url.create({ shortUrl, originalUrl: targetURL })
-    )
-    .then(data =>
-      res.render("success", {
-        originalUrl: req.body.inputURL,
-        shortUrl: data.shortUrl,
-      })
-    )
-    .catch(error => console.error(error))
-})
-
-app.get('/:shortUrl', (req, res) => {
-  const shortUrl = host + req.params.shortUrl 
-
-  Url.findOne({ shortUrl })
-    .then(data => {
-      if (!data) {
-        return res.render("error", {
-          errorMsg: "Can't found the URL",
-          errorURL: shortUrl,
-        })
-      }
-      res.redirect(data.originalUrl)
-    })
-    .catch(error => console.error(error))
-})
 
 
 app.listen(3000, () => {
---
+++ routes/router.js
@@ -0,0 +1,45 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+const Url = require('../models/url')
+const generatePath = require('../public/javascripts/generate_shortURL')
+const host = 'www.localhost:3000/'
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.post('/shortened', (req, res) => {
+  const targetURL = req.body.inputURL
+  const shortUrl = host + generatePath(5)
+  Url.findOne({ originalUrl: targetURL })
+    .then(data =>
+      data ? data : Url.create({ shortUrl, originalUrl: targetURL })
+    )
+    .then(data =>
+      res.render("success", {
+        originalUrl: req.body.inputURL,
+        shortUrl: data.shortUrl,
+      })
+    )
+    .catch(error => console.error(error))
+})
+
+router.get('/:shortUrl', (req, res) => {
+  const shortUrl = host + req.params.shortUrl
+
+  Url.findOne({ shortUrl })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: shortUrl,
+        })
+      }
+      res.redirect(data.originalUrl)
+    })
+    .catch(error => console.error(error))
+})
+
+
+module.exports = router
